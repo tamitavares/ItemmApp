a36747bffe0895797c93adc37989f5bb630a0a37
Camila Jardim <camila.dfj@hotmail.com>
2023-11-12 14:53:10 -0300

    Created a dropdown list to have a download of certificates

--- src/views/jovem/CertificadoJovem.jsx
+++ src/views/jovem/CertificadoJovem.jsx
@@ -1,47 +1,71 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, Button, Alert } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Image, StyleSheet, Button, Alert, Linking } from 'react-native';
+import { getStorage, ref, getDownloadURL, listAll } from "firebase/storage";
+import RNPickerSelect from 'react-native-picker-select';
 
 export default CertificadoJovem = () => {
 
-  const solicitarCertificado = () => {
-    Alert.alert("Solicitação de certificado executada")
-  };
-  const baixarCertificado = () => {
-    Alert.alert("Certificado baixado")
-  };
+  const storage = getStorage();
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [dropdownOptions, setDropdownOptions] = useState([]);
 
+  useEffect(() => {
+    const carregarOpcoesDropdown = async () => {
+      try {
+        const storageRef = ref(storage);
+        const listaArquivos = await listAll(storageRef);
+        const opcoes = listaArquivos.items.map(item => ({ label: item.name, value: item.name }));
+        setDropdownOptions(opcoes);
+      } catch (error) {
+        console.error('Erro ao carregar opções de dropdown:', error);
+      }
+    };
+  carregarOpcoesDropdown();
+  }, []);
+
+  const baixarCertificado = async () => {
+    if (selectedOption) {
+      const refCertificado = ref(storage, selectedOption.value);
+      const downloadURL = await getDownloadURL(refCertificado);
+      Linking.openURL(downloadURL).catch((err) => console.error('Erro ao abrir o PDF:', err));
+      Alert.alert("Certificado baixado");
+    } else {
+      Alert.alert('Selecione uma opção antes de baixar o certificado');
+    }
+  };
 
   return (
     <View style={styles.certificadoJovem}>
-      <View style={styles.container}>
-        <Image
-          style={styles.image}
-          source={require('../images/logo.png')}
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('../images/logo.png')}
+      />
+
+      <View style={styles.dropdown}>
+        <RNPickerSelect
+          onValueChange={(value) => setSelectedOption({ label: value, value })}
+          items={dropdownOptions}
+          placeholder={{ label: 'Selecione uma opção', value: null }}
         />
-        
-        <View style={styles.button}>
-          <Button
-          onPress={solicitarCertificado}
-          title="Solicitar certificado"
-          color='#263868'
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
+      </View>
+
+      <View style={styles.button}>
+        <Button
           onPress={baixarCertificado}
           title="Baixar certificado"
           color='#263868'
-          />
-        </View>
-        <Text style={styles.logout}>Sair</Text>
-        <Text style={styles.title}>Emissão de Certificado</Text>
-        <Text style={styles.titlePrincipal}>
-          A emissão do seu certificado será feita pelo ITEMM. Aguarde a notificação em sua caixa de entrada.
-          {'\n'}
-          Caso não tenha recebido ainda envie uma solicitação.
-        </Text>
+        />
       </View>
+
+      <Text style={styles.logout}>Sair</Text>
+      <Text style={styles.title}>Emissão de Certificado</Text>
+      <Text style={styles.titlePrincipal}>
+        Escolha o certificado que deseja baixar com base nas opções disponíveis:
+      </Text>
+    
     </View>
+  </View>
   );
 };
 
@@ -50,6 +74,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecfddc',
     flex: 1,
     alignItems: 'center',
+  },
+  dropdown: {
+    display: 'flex',
+    height: 40,
+    borderRadius: 8,
+    marginHorizontal: 16,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    marginTop: 350,
+    justifyContent:'space-evenly',
   },
   container: {
     backgroundColor: '#ecfddc',
@@ -66,16 +100,15 @@ const styles = StyleSheet.create({
     width: 179,
   },
   button: {
+    display:'flex',
     backgroundColor: '#263868',
     height: 53,
     width: 200,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    top: 210,
     borderRadius: 10,
     marginBottom: 10,
     left: 68,
-    top: 468
   },
   buttonText: {
     color: '#ffffff',
